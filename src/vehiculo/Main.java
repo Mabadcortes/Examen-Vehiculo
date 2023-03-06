c0038c80f738ff75788b940f4a11e5ccd7d75509
Usuario <Usuario@ManuAbad>
2023-03-06 09:28:47 +0100

    Javadoc del vehiculo

--- src/vehiculo/Main.java
+++ src/vehiculo/Main.java
@@ -6,29 +6,37 @@ package vehiculo;
  */
 public class Main {
 
-    private static void OperativaVehiculosACM2223(VehiculoACM2223 miVehiculoXXX2223, int cantidad) {
+    /**
+     * Esta función muestra la venta y compra de vehículos y el stock actual de vehículos
+     * @param miVehiculoACM2223 objeto vehiculo
+     * @param cantidad cantidad de vehiculos en stock
+     */
+    private static void operativaVehiculosACM2223(VehiculoACM2223 miVehiculoACM2223, int cantidad) {
         int stockActual;
         try {
             System.out.println("Venta de Vehiculos");
-            miVehiculoXXX2223.vender(20);
+            miVehiculoACM2223.vender(20);
         } catch (Exception e) {
             System.out.print("Fallo al vender");
         }
         try {
             System.out.println("Compra de Vehiculos");
-            miVehiculoXXX2223.comprar(100);
+            miVehiculoACM2223.comprar(100);
         } catch (Exception e) {
             System.out.print("Fallo al comprar");
         }
-        stockActual = miVehiculoXXX2223.obtenerStock();
+        stockActual = miVehiculoACM2223.obtenerStock();
         System.out.println("El stock actual es " + stockActual);
     }
-
+/**
+ * 
+ * El main crea el objeto vehículo y llama a la función OperativaVehiculosACM2223
+ */
     public static void main(String[] args) {
-        VehiculoACM2223 miVehiculoXXX2223;
+        VehiculoACM2223 miVehiculoACM2223;
 
-        miVehiculoXXX2223 = new VehiculoACM2223("Seat", 18000, 100);
-        OperativaVehiculosACM2223(miVehiculoXXX2223, 50);
+        miVehiculoACM2223 = new VehiculoACM2223("Seat", 18000, 100);
+        operativaVehiculosACM2223(miVehiculoACM2223, 50);
     }
 
 }
--- src/vehiculo/VehiculoACM2223.java
+++ src/vehiculo/VehiculoACM2223.java
@@ -1,9 +1,8 @@
-
 package vehiculo;
 
 /**
  *
- * @author 
+ * @author Manu Abad
  */
 public class VehiculoACM2223 {
 
@@ -12,88 +11,131 @@ public class VehiculoACM2223 {
     private double precioIVA;
     private int stock;
 
-    /* Constructor sin argumentos */
-    public VehiculoACM2223 ()
-    {
+    /**
+     * Constructor sin parámetros
+     */
+    public VehiculoACM2223() {
     }
-    // Constructor con parámetro para iniciar todas las propiedades de la clase
-    
-    
-    public VehiculoACM2223 (String nom, double precio, int stock)
-    {
-        this.nombre =nom;
-        this.precio=precio;
-        this.stock=stock;
+
+    /**
+     * Constructor con parámetros para iniciar todas las propiedades de la clase
+     *
+     * @param nom nombre del vehículo
+     * @param precio precio del vehículo
+     * @param stock stock del vehículo
+     */
+    public VehiculoACM2223(String nom, double precio, int stock) {
+        this.nombre = nom;
+        this.precio = precio;
+        this.stock = stock;
     }
-   // Método para asignar el nombre del vehiculo
-    public void asignarNombre(String nom)
-    {
-        nombre=nom;
+
+    /**
+     * Método para asignar el nombre del vehiculo
+     *
+     * @param nom nombre del vehículo
+     */
+    public void asignarNombre(String nom) {
+        nombre = nom;
     }
-    // Método que me devuelve el nombre del vehiculo
-    public String obtenerNombre()
-    {
+
+    /**
+     * Método que me devuelve el nombre del vehiculo
+     *
+     * @return nombre del vehículo
+     */
+    public String obtenerNombre() {
         return nombre;
     }
 
-    // Método que me devuelve el stock de vehiculos disponible en cada momento
-     public int obtenerStock ()
-    {
+    /**
+     *Método que me devuelve el stock de vehiculos disponible en cada momento
+     * @return stock de vehículos
+     */
+    public int obtenerStock() {
         return stock;
     }
-
-    /* Método para comprar vehiculos. Modifica el stock.
-     * Este método va a ser probado con Junit
-     */
-    public void comprar(int cantidad) throws Exception
-    {
-        if (cantidad<0)
+/**
+ * Método para comprar vehiculos. Modifica el stock.
+ * Este método va a ser probado con Junit
+ * @param cantidad cantidad de vehículos
+ * @throws Exception muestra una excepción
+ */
+    public void comprar(int cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede comprar un nº negativo de vehiculos");
+        }
         stock = stock + cantidad;
     }
-
-    public void vender (int cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede vender una cantidad negativa de vehiculos");
-        if (obtenerStock()< cantidad)
-            throw new Exception ("No se hay suficientes vehiculos para vender");
+/**
+ *  Método para vender vehículos. Modifica el stock
+ * @param cantidad cantidad de vehículos
+ * @throws Exception muestra una excepción
+ */
+    public void vender(int cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede vender una cantidad negativa de vehiculos");
+        }
+        if (obtenerStock() < cantidad) {
+            throw new Exception("No se hay suficientes vehiculos para vender");
+        }
         stock = stock - cantidad;
     }
-
+/**
+ * Muestra el valor del nombre del vehiculo
+ * @return nombre del vehiculo
+ */
     public String getNombre() {
         return nombre;
     }
-
+/**
+ * Permite cambiar el nombre del vehiculo
+ * @param nombre nombre del vehiculo
+ */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+/**
+ * Muestra el valor del precio del vehiculo
+ * @return el precio del vehiculo
+ */
     public double getPrecio() {
         return precio;
     }
-
+/**
+ * Permite cambiar el precio del vehiculo
+ * @param precio precio del vehiculo
+ */
     public void setPrecio(double precio) {
         this.precio = precio;
     }
-
+/**
+ * Muestra el valor del precioIVA
+ * @return devuelve el valor de precioIVA
+ */
     public double getPrecioIVA() {
         return precioIVA;
     }
-
+/**
+ * Permite cambiar el valor de precioIVA
+ * @param precioIVA precio del IVA
+ */
     public void setPrecioIVA(double precioIVA) {
         this.precioIVA = precioIVA;
     }
-
+/**
+ * Muestra el stock de vehiculos
+ * @return devuelve el stock de vehiculos
+ */
     public int getStock() {
         return stock;
     }
-
+/**
+ * Permite cambiar la cantidad de vehiculos en stock
+ * @param stock devuelve el stock de vehiculos
+ */
     public void setStock(int stock) {
         this.stock = stock;
     }
-    
-}  
-   
-    
 
+}
